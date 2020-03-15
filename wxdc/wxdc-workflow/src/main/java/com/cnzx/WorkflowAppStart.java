b5d350e987bf4446b1bd9b26d560459da3bbdf92
linxiaoping <137708567@qq.com>
2020-03-15 23:06:51 +0800

    项目初始化

---
+++ wxdc/wxdc-workflow/src/main/java/com/cnzx/WorkflowAppStart.java
@@ -0,0 +1,13 @@
+package com.cnzx;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowAppStart {
+
+     public static void main(String[] args) {
+         SpringApplication.run(WorkflowAppStart.class,args);
+    }
+}
